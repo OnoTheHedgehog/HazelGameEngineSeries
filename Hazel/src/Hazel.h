6dabf9aa1f81afd8223f15cf081ca8cb42e6ff1f
Onome Oyibo <onome09@hotmail.com>
2019-12-10 20:07:07 +0000

    added ImGui

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "hzpch.h"
 
-
 // For use by Hazel applications
 #include "Hazel/Application.h"
 #include "Hazel/Log.h"
 #include "Hazel/Layer.h"
+#include "Hazel/ImGui/ImGuiLayer.h"
 //Entry point
 #include "Hazel/EntryPoint.h"
 
