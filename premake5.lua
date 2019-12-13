34a9c288af8b64197ef146d6f5b7099356693538
Onome Oyibo <onome09@hotmail.com>
2019-12-13 00:34:25 +0000

    correct startup project and make dist directories is non exist in postbuild commands

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,6 @@
 workspace "Hazel"
 	architecture "x64"
-
+	startproject "Sandbox"
 	configurations
 	{
 		"Debug",
@@ -19,8 +19,6 @@ IncludeDir["ImGui"] = "Hazel/vendor/imgui"
 include "Hazel/vendor/GLFW"
 include "Hazel/vendor/Glad"
 include "Hazel/vendor/imgui"
-
-startproject "Sandbox"
 
 project "Hazel"
 	location "Hazel"
@@ -70,7 +68,7 @@ project "Hazel"
 
 		postbuildcommands 
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 
 	filter "configurations:Debug"
